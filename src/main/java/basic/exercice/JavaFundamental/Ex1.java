367baff80f03b1758e2b61728a31f19a202b5906
Maria Abreu@Eduarda <mariaeduardaabreu005@gmail.com>
2025-11-28 16:40:16 -0300

    make the exercice num 1 of curse DIO with CI&T

---
+++ src/main/java/basic/exercice/JavaFundamental/Ex1.java
@@ -0,0 +1,16 @@
+/* exercice num 1
+
+Description: Write a code reception the name and the year of born for somebody and output in screen
+the message: "Hello 'name the person' you are 'X' years old"
+
+ */
+
+package basic.exercice;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello" + );
+    }
+}
